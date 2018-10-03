ec1f1a221f4400bd57f7953c4af55a8fb42d67d0
yc1307 <JJYC1307@163.com>
2018-10-03 08:48:04 +0800

    an weights-baises-example

---
+++ first_demo.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2018/10/2 19:18
+# @Author  : yc1307
+# @File    : first_demo.py
+import tensorflow as tf
+import numpy as np
+import os
+# 设置 TensorFlow 的 Log 输出级别
+# 1 默认等级，显示所有信息
+# 2 只显示 warning 和 Error
+# 3 只显示 Error
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+
+x_data = np.random.rand(100).astype(np.float)
+y_data = x_data * 0.1 + 0.3
+
+# create TensorFlow structure
+weights = tf.Variable(tf.random_uniform([1], -1.0, 1.0))
+biases = tf.Variable(tf.zeros([1]))
+
+y = weights * x_data + biases
+
+loss = tf.reduce_mean(tf.square(y - y_data))
+optimizer = tf.train.GradientDescentOptimizer(0.5)
+train = optimizer.minimize(loss)
+
+init = tf.global_variables_initializer()
+
+# instantiation
+sess = tf.Session()
+sess.run(init)
+
+for step in range(201):
+    sess.run(train)
+    if step % 20 == 0:
+        print(step, sess.run(weights), sess.run(biases))
+
